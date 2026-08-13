0f68ee139c88fd7c970ecad8ca4c6130ec65e356
Sanika-2212 <sanikamisal26@gmail.com>
2026-08-13 23:29:42 +0530

    Add files via upload

---
+++ CaseStudyStep3.py
@@ -0,0 +1,68 @@
+import pandas as pd
+
+Border = "-"*30
+
+##############################################################
+# Step 1 : Load the dataset
+##############################################################
+
+print(Border)
+print("Step 1 : Load the dataset")
+print(Border)
+
+DataPath = "iris.csv"
+
+df =pd.read_csv(DataPath)         #df = dataframe
+
+print("Dataset loaded successfully")
+
+print("Initial Entries from dataset are:")
+print(df.head())        #first 5 entries
+
+print("Last Entries from dataset are:")
+print(df.tail())        #last 5 entries
+
+##############################################################
+# Step 2 : Exploratory Data Analysis (EDA)
+##############################################################
+
+print(Border)
+print("Step 2 : Exploratory Data Analysis (EDA)")
+print(Border)
+
+print("Shape of dataset",df.shape)
+
+print("Column Names:",list(df.columns))
+
+print("Missing Values per column:")
+print(df.isnull().sum())
+
+print("Class Distribution (species count):")
+print(df["species"].value_counts())
+
+print("Statistical report of dataset:")
+print(df.describe())
+
+##############################################################
+# Step 3 : Decide Independent and Dependent Variables
+##############################################################
+
+print(Border)
+print("Step 3 : Decide Independent and Dependent Variables")
+print(Border)
+
+# X : Independent Variable / Features
+# Y : Dependent Variable   / Labels
+
+feature_cols = [
+    "sepal length (cm)",
+    "sepal width (cm)",
+    "petal length (cm)",
+    "petal width (cm)"
+    ]
+
+X = df[feature_cols]
+Y = df["species"]
+
+print("X shape",X.shape)
+print("Y shape",Y.shape)
